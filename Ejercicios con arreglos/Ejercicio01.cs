39d2ed7aec2bdf09a5d8b0a2fc5f0d811ea2fb3e
jinnie0 <jinniesoto@gmail.com>
2025-02-10 21:52:03 -0400

    Add files via upload

---
+++ Ejercicios con arreglos/Ejercicio01.cs
@@ -0,0 +1,34 @@
+﻿// Leer 10 enteros, almacenarlos en un arreglo y determinar en qué posición del arreglo está el mayor número leído.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[10];
+
+        // Leer 10 números enteros
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Ingrese el número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Encontrar el mayor número y su posición
+        int mayorNumero = numeros[0];
+        int posicionMayor = 0;
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (numeros[i] > mayorNumero)
+            {
+                mayorNumero = numeros[i];
+                posicionMayor = i;
+            }
+        }
+
+        // Mostrar el resultado
+        Console.WriteLine($"El mayor número es {mayorNumero} y está en la posición {posicionMayor} del arreglo.");
+    }
+}
