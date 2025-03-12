80eeeac1a96cc31273c05ede967838dfa9633604
jinnie0 <jinniesoto@gmail.com>
2025-03-12 17:18:31 -0400

    Add files via upload

--- Práctica Polimorfismo/ConsoleApp1/Program.cs
+++ Práctica Polimorfismo/ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pinguino pinguino = new Pinguino("Emperador", "Grande", "Antártida");
+        Aguila aguila = new Aguila("Calva", "Mediana", "América del Norte");
+        
+        Ave[] aves = { pinguino, aguila };
+        
+        Console.WriteLine("Comportamiento de vuelo de las aves:");
+        foreach (Ave ave in aves)
+        {
+            Console.WriteLine();
+        }
+        
+        Console.ReadKey();
+    }
+}
